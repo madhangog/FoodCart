9dff2f2f70ce4a6ac41d8f1a681b65971b374606
maddy <madhanmanoj2017@gmail.com>
2019-11-30 12:14:07 +0530

    Updates

---
+++ precache-manifest.f90351b5ea60925115f4d88a07bb3518.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c509357a0e709d0530c146f3cbd4a89e",
+    "url": "/foodcart/index.html"
+  },
+  {
+    "revision": "61e4ac98d3c5eb498b7f",
+    "url": "/foodcart/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "87de1f2e62591894f422",
+    "url": "/foodcart/static/css/main.89c21ff1.chunk.css"
+  },
+  {
+    "revision": "61e4ac98d3c5eb498b7f",
+    "url": "/foodcart/static/js/2.f800d462.chunk.js"
+  },
+  {
+    "revision": "87de1f2e62591894f422",
+    "url": "/foodcart/static/js/main.72309034.chunk.js"
+  },
+  {
+    "revision": "3f3de7e20054c63c12df",
+    "url": "/foodcart/static/js/runtime~main.a160ed5c.js"
+  },
+  {
+    "revision": "54b20fbabf283c4ef98c3f3a6736952f",
+    "url": "/foodcart/static/media/images (1).54b20fba.jpeg"
+  },
+  {
+    "revision": "4c541f4e865ad2c8fca981926fb3a92d",
+    "url": "/foodcart/static/media/images.4c541f4e.jpeg"
+  },
+  {
+    "revision": "3fd7965b95799ef4200613669ea32125",
+    "url": "/foodcart/static/media/noodles.3fd7965b.jpg"
+  }
+]);
